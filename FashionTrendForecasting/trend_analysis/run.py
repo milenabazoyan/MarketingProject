f8ce67cd6ed4ecbcce3ca3aae553a00ba224d254
SateA29 <160149055+SateA29@users.noreply.github.com>
2024-04-26 15:54:08 +0400

    run.py

--- FashionTrendForecasting/trend_analysis/run.py
+++ FashionTrendForecasting/trend_analysis/run.py
@@ -1,5 +1,7 @@
 from FashionTrendForecasting.trend_analysis.model import *
 from FashionTrendForecasting.data_processing.sql_interactions import Interactions
+from FashionTrendForecasting.trend_analysis.database_manager import add_predictions_to_database
+from FashionTrendForecasting.trend_analysis.config import *
 import pandas as pd
 
 # Define the new data
@@ -16,4 +18,4 @@ sql_interactions_instance = Interactions()
 df = sql_interactions_instance.get_sales_volume()
 X_train, y_train = split_data(df, 'trend_score')
 y_pred = train_and_predict(X_train, y_train, new_data)
-print(y_pred)
+add_predictions_to_database(DATABASE_PATH,y_pred)

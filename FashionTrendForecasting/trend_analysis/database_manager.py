baa35401b5443d217d72fd804bc17f7e35317782
SateA29 <160149055+SateA29@users.noreply.github.com>
2024-04-26 15:53:32 +0400

    database_manager.py

--- FashionTrendForecasting/trend_analysis/database_manager.py
+++ FashionTrendForecasting/trend_analysis/database_manager.py
@@ -1,19 +1,26 @@
-# database_manager.py
-import sqlite3
-import pandas as pd
-from FashionTrendForecasting.data_processing.sql_interactions import Interactions
 from FashionTrendForecasting.trend_analysis.config import DATABASE_PATH
+import sqlite3
 
-def create_connection():
-    conn = sqlite3.connect(DATABASE_PATH)
-    return conn
 
-def insert_predictions(data):
-    conn = create_connection()
-    data.to_sql('predictions', conn, if_exists='append', index=False)
-    conn.close()
+def add_predictions_to_database(database_path, predicted_data):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
 
-def fetch_data():
-    interactions_instance = Interactions()
-    df = interactions_instance.get_sales_volume()
-    return df
+    try:
+        # Add a new column named "Predictions" to the "Item" table
+        cursor.execute("ALTER TABLE Item ADD COLUMN Predictions REAL")
+
+        # Update the "Predictions" column with the predicted data
+        for index, row in predicted_data.iterrows():
+            cursor.execute("UPDATE Item SET Predictions = ? WHERE item_id = ?", (row['Predictions'], row['item_id']))
+
+        # Commit the changes to the database
+        conn.commit()
+        print("Predictions added to the database successfully.")
+    except sqlite3.Error as e:
+        print("Error:", e)
+    finally:
+        conn.close()
+
+
+

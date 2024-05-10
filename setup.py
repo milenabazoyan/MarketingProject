2aa7ebfab50468876bda3375fa61c234263476ec
SateA29 <160149055+SateA29@users.noreply.github.com>
2024-05-10 10:19:00 +0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-#TODO
+from setuptools import setup, find_packages
+setup(
+    author = 'Anahit Manukyan, Milena Bazoyan, Lusine Torosyan, Sate Antaranyan',
+    description= 'A package for predicting fashion trends',
+    name = 'EcoTrendz',
+    version = '0.1.0',
+    packages = find_packages(include = ['FashionTrendForecasting', 'FashionTrendForecasting.*'])
+)

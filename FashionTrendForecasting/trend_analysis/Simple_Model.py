edd2ce3fd9ebadd1b5bf3e44b0e543b2736f9f6c
SateA29 <160149055+SateA29@users.noreply.github.com>
2024-05-11 18:40:23 +0400

    Simple_Model.py

--- FashionTrendForecasting/trend_analysis/Simple_Model.py
+++ FashionTrendForecasting/trend_analysis/Simple_Model.py
@@ -1,26 +1,33 @@
-from FashionTrendForecasting.data_processing.sql_interactions import Interactions
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
+def train_random_forest(df):
+    X = df[['category', 'material', 'style', 'color']]
+    y = df['trend_score']
+    X = pd.get_dummies(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    rf = RandomForestRegressor(n_estimators=100, random_state=42)
+    rf.fit(X_train, y_train)
+    
+    y_pred = rf.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    
+    return rf, mse
 
-sql_interactions_instance = Interactions()
-df = sql_interactions_instance.get_detailed_item_trends()
-
-X = df[['category', 'material', 'style', 'color']]
-y = df['trend_score']
-X = pd.get_dummies(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-rf = RandomForestRegressor(n_estimators=100, random_state=42)
-rf.fit(X_train, y_train)
-
-y_pred = rf.predict(X_test)
-dt = DecisionTreeRegressor(random_state=42)
-dt.fit(X_train, y_train)
-y_pred_dt = dt.predict(X_test)
-mse_dt = mean_squared_error(y_test, y_pred_dt)
-mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error for Decision Tree: {mse_dt}")
-print(f"Mean Squared Error for Random Forest: {mse}")
+def train_decision_tree(df):
+    X = df[['category', 'material', 'style', 'color']]
+    y = df['trend_score']
+    X = pd.get_dummies(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    dt = DecisionTreeRegressor(random_state=42)
+    dt.fit(X_train, y_train)
+    
+    y_pred = dt.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    
+    return dt, mse

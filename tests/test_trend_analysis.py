0e1cf1c96cb87169b0beefeb09407d8f8c0fbde1
SateA29 <160149055+SateA29@users.noreply.github.com>
2024-05-11 19:46:40 +0400

    Update test_trend_analysis.py

--- tests/test_trend_analysis.py
+++ tests/test_trend_analysis.py
@@ -1,45 +1,15 @@
-from FashionTrendForecasting.trend_analysis.model import *
-from sklearn.model_selection import train_test_split
+from FashionTrendForecasting.trend_analysis.Simple_Model import *
+
 from FashionTrendForecasting.data_processing.sql_interactions import Interactions
-import matplotlib.pyplot as plt
-from sklearn.metrics import mean_absolute_error
 
-def split_data(data, target_feature, test_size=0.2, random_state=42):
-    # Separate target variable
-    y = data[target_feature]
+interactions = Interactions()
+df = interactions.get_sales_volume()
+rf_model, rf_mse = train_random_forest(df, 0.2, 42)
 
-    # Extract features
-    X = data.drop(columns=[target_feature])
+# Print the mean squared error
+print("Mean Squared Error:", rf_mse)
 
-    # Identify categorical features
-    categorical_features = X.select_dtypes(include=['object']).columns.tolist()
-
-    # One-hot encode categorical features
-    X_encoded = pd.get_dummies(X, columns=categorical_features, drop_first=True)
-
-    # Split the data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=test_size, random_state=random_state)
-
-    return X_train, X_test, y_train, y_test
-
-
-sql_interactions_instance = Interactions()
-df = sql_interactions_instance.get_sales_volume()
-X_train, X_test, y_train, y_test = split_data(df, 'trend_score')
-y_pred = train_and_predict(X_train, y_train, X_test)
-
-
-def mae(y_test, y_pred):
-    return mean_absolute_error(y_test, y_pred)
-
-mae_value = mae(y_test, y_pred)
-print("MAE:", mae_value)
-
-plt.figure(figsize=(8, 6))
-plt.scatter(y_test, y_pred, color='blue', label='Predicted')
-plt.scatter(y_test, y_test, color='red', label='Actual')
-plt.title('Actual vs. Predicted')
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
-plt.legend()
-plt.show()
+dt_model, dt_mse = train_decision_tree(df, 0.2, 42)
+print("Mean Squared Error:", dt_mse)
+gb_model, gb_mse = train_gradient_boosting(df, 0.2, 42)
+print("Mean Squared Error:",gb_mse)
